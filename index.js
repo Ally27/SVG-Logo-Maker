b04cea0239d2e2e0a4820101e96d205de4e1202e
Allyson Gonzales <Allysonmg21@gmail.com>
2023-03-12 15:12:42 -0700

    Added more questions and "userData"

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
-const inquirer=require("inquirer");
-const fs=require("fs");
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 inquirer
-.prompt([{
-    type:"input",
-    name:"logoInfo",
-    message:"Enter the Logo Name",
-},
-{
-    type:"input",
-    name:"textColor",
-    message:"Enter the text color",
-},
-{
-    type:"input",
-    name:"logoColor",
-    message:"Enter the logo color",
-},
-{
-    
-}
-])
+  .prompt([
+    {
+      type: "input",
+      name: "logoInfo",
+      message: "Enter the Logo Name",
+    },
+    {
+      type: "input",
+      name: "textColor",
+      message: "Enter the text color",
+    },
+    {
+      type: "input",
+      name: "logoColor",
+      message: "Enter the logo color",
+    },
+    {
+      type: "input",
+      name: "shapeType",
+      message: "Select a shape:",
+      choices: ["Square", "Triangle", "Circle"],
+    },
+  ])
+  .then((userData) => {
+    const svgPath = "";
+    const finalLogo = formShape(userData);
+    fs.writeToFile(svgPath, generateSVG(finalLogo), (err) =>
+      err ? console.error(err) : console.info("Generating Logo.svg")
+    );
+  })
+  .catch((err) => console.error(err));
